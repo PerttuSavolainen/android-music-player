8027e54ee98edd58795dfc32cd940587b43fed43
perttu <savolainen.perttu@gmail.com>
2016-10-02 11:32:48 +0300

    music player tehty

---
+++ app/src/main/java/fi/jamk/musicplayer/MainActivity.java
@@ -0,0 +1,116 @@
+package fi.jamk.musicplayer;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    // listview
+    private ListView listview;
+    // path to music files
+    private String mediaPath;
+    // list of strings to hold filenames
+    private List<String> songs = new ArrayList<String>();
+    // mediaplayer
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+    //private MediaPlayer mediaPlayer;
+
+    // asyncTask to load filenames
+    private LoadSongTask task;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listview = (ListView) findViewById(R.id.listView);
+        mediaPath = "/storage/extSdCard/Music/";
+
+        //mediaPlayer = MediaPlayer.create(MainActivity.this, Uri.parse(songs.get(0)));
+        mediaPlayer = new MediaPlayer();
+
+        // item listener
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                try {
+                    mediaPlayer.reset();
+                    mediaPlayer.setDataSource(songs.get(position));
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                } catch (IOException e) {
+                    Toast.makeText(getBaseContext(), "Can't start audio...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        task = new LoadSongTask();
+        task.execute();
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mediaPlayer.isPlaying()) mediaPlayer.release();
+    }
+
+
+
+    private class LoadSongTask extends AsyncTask<Void, String, Void> {
+        private List<String> loadedSongs = new ArrayList<String>();
+
+        @Override
+        protected Void doInBackground(Void... url) {
+            updateSongListRecursive(new File(mediaPath));
+            return null;
+        }
+
+        protected void onPreExecute() {
+            Toast.makeText(getApplicationContext(), "Loading...", Toast.LENGTH_LONG).show();
+        }
+
+        public void updateSongListRecursive(File path) {
+            if (path.isDirectory()) {
+                for (int i=0; i<path.listFiles().length; i++) {
+                    File file = path.listFiles()[i];
+                    updateSongListRecursive(file);
+                }
+            } else {
+                String name = path.getAbsolutePath();
+                publishProgress(name);
+                if (name.endsWith(".mp3")) {
+                    loadedSongs.add(name);
+                }
+            }
+        }
+
+        protected void onPostExecute(Void args) {
+            ArrayAdapter<String> songList = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, loadedSongs);
+            listview.setAdapter(songList);
+            songs = loadedSongs;
+
+            Toast.makeText(getApplicationContext(), "Songs = " + songs.size(), Toast.LENGTH_LONG).show();
+
+        }
+
+    }
+}
